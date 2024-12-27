164057e58db0a5d4f5f43a520bfdef7436bf3616
Abhishek <cabhishek660@gmail.com>
2024-12-27 10:44:20 -0500

    Adding LinkedIn link.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,49 +15,84 @@ import MSExpertBadge from "../icons/microsoft-certified-expert-badge.svg";
 import D365Logo from "../icons/dynamics365-svg.svg";
 import Constants from "../constants.json";
 import ProfilPhoto from "../icons/profile-photo.jpg";
+import "../styles/LinkedInButton.css";
 
 const About = () => {
-    return (<div id="about">
-        <div className="about-section">
-            <div className="about-text">
-                <h1 className="title">Software developer</h1>
-                <p>
-                    Results-driven software developer with experience in designing, developing, and deploying high-quality applications that address complex business and user needs.
-                </p>
-                <div className="certs-container">
-                    <div className="cert-icon" onClick={()=>{
-                        window.open(Constants.DevAssociateBadgeUrl,"_blank");
-                    }} title="Microsoft Certified: Power Platform Developer Associate">
-                        <MSAssociateBadge />
-                    </div>
-                    <div className="cert-icon" onClick={()=>{
-                        window.open(Constants.SAExpertBadgeUrl,"_blank");
-                    }} title="Microsoft Certified: Power Platform Solution Architect Expert">
-                        <MSExpertBadge />
-                    </div>
-                </div>
-                <div className="skills-container">
-                    <h4 className="skills-title">Skills</h4>
-                    <div className="skills">
-                        <div className="skill-icon" title=".NET"><NetLogo /></div>
-                        <div className="skill-icon" title="C#"><CsharpLogo /></div>
-                        <div className="skill-icon" title="Microsoft Azure"><AzureLogo /></div>
-                        <div className="skill-icon" title="Azure Functions"><AzureFuncLogo /></div>
-                        <div className="skill-icon" title="React"><ReactLogo /></div>
-                        <div className="skill-icon" title="Microsoft Dynamics 365"><D365Logo /></div>
-                        <div className="skill-icon" title="JavaScript"><JavaScriptLogo /></div>
-                        <div className="skill-icon" title="TypeScript"><TypeScriptLogo /></div>
-                        <div className="skill-icon" title="Power Apps"><PowerAppsLogo /></div>
-                        <div className="skill-icon" title="Power Automate"><PowerAutomateLogo /></div>
-                        <div className="skill-icon" title="SQL"><SQLLogo /></div>
-                    </div>
-                </div>
+  return (
+    <div id="about">
+      <div className="about-section">
+        <div className="about-text">
+          <h1 className="title">Software developer</h1>
+          <p>
+            Results-driven software developer with experience in designing,
+            developing, and deploying high-quality applications that address
+            complex business and user needs.
+          </p>
+          <div className="certs-container">
+            <div
+              className="cert-icon"
+              onClick={() => {
+                window.open(Constants.DevAssociateBadgeUrl, "_blank");
+              }}
+              title="Microsoft Certified: Power Platform Developer Associate"
+            >
+              <MSAssociateBadge />
             </div>
-            <div className="about-image">
-                <img src={ProfilPhoto} alt="Profile" />
+            <div
+              className="cert-icon"
+              onClick={() => {
+                window.open(Constants.SAExpertBadgeUrl, "_blank");
+              }}
+              title="Microsoft Certified: Power Platform Solution Architect Expert"
+            >
+              <MSExpertBadge />
             </div>
+          </div>
+          <div className="skills-container">
+            <h4 className="skills-title">Skills</h4>
+            <div className="skills">
+              <div className="skill-icon" title=".NET">
+                <NetLogo />
+              </div>
+              <div className="skill-icon" title="C#">
+                <CsharpLogo />
+              </div>
+              <div className="skill-icon" title="Microsoft Azure">
+                <AzureLogo />
+              </div>
+              <div className="skill-icon" title="Azure Functions">
+                <AzureFuncLogo />
+              </div>
+              <div className="skill-icon" title="React">
+                <ReactLogo />
+              </div>
+              <div className="skill-icon" title="Microsoft Dynamics 365">
+                <D365Logo />
+              </div>
+              <div className="skill-icon" title="JavaScript">
+                <JavaScriptLogo />
+              </div>
+              <div className="skill-icon" title="TypeScript">
+                <TypeScriptLogo />
+              </div>
+              <div className="skill-icon" title="Power Apps">
+                <PowerAppsLogo />
+              </div>
+              <div className="skill-icon" title="Power Automate">
+                <PowerAutomateLogo />
+              </div>
+              <div className="skill-icon" title="SQL">
+                <SQLLogo />
+              </div>
+            </div>
+          </div>
         </div>
-    </div>)
-}
+        <div className="about-image">
+          <img src={ProfilPhoto} alt="Profile" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
